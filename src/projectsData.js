bb14c8cff035df99a338b024ff683ad4ad72ff97
amaechinwankwo <damaechi@rocketmail.com>
2024-12-13 23:35:34 +0100

    my third commit

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,112 +1,90 @@
+import nitadImage from "../src/assets/nitad.png";
+import producemart from "../src/assets//producemart.png";
+import primeparkland from "../src/assets/primeparkland.png";
+import crypto from "../src/assets/crypto.png";
+import dashboard from "../src/assets/dashboard.png";
+import dashboard1 from "../src/assets/dashboard1.png";
+import inputverify from "../src/assets/inputverify.png";
+import nextedreg from "../src/assets/nextedreg.png";
+import digitalarch from "../src/assets/digitalarch.png";
+
+import html from "../src/assets/html.jpeg";
+import css from "../src/assets/css.jpeg";
+import js from "../src/assets/js.jpeg";
+import bootstrap from "../src/assets/bootstrap.jpeg";
+
 const projects = [
   {
     id: 1,
     name: "Landing page of NITAD website",
-    photo: "/src/assets/nitad.png",
+    photo: nitadImage,
     code: "https://github.com/dandytech/NITAD",
     site: "https://dandy-nitad.netlify.app/",
-    stack: [
-      "/src/assets/html.jpeg",
-      "/src/assets/css.jpeg",
-      "/src/assets/js.jpeg",
-      "/src/assets/bootstrap.jpeg",
-    ],
+    stack: [html, css, js, bootstrap],
   },
   {
     id: 2,
     name: "Landing page of Producemart website",
-    photo: "/src/assets/producemart.png",
+    photo: producemart,
     code: "https://github.com/dandytech/producemart",
     site: "https://dandy-producemart.netlify.app/",
-    stack: [
-      "/src/assets/html.jpeg",
-      "/src/assets/css.jpeg",
-      "/src/assets/js.jpeg",
-      "/src/assets/bootstrap.jpeg",
-    ],
+    stack: [html, css, js, bootstrap],
   },
   {
     id: 3,
     name: "Landing page of Primeparkland website",
-    photo: "/src/assets/primeparkland.png",
+    photo: primeparkland,
     code: "https://github.com/dandytech/primeparkland",
     site: "https://dandy-primeparkland.netlify.app/",
-    stack: [
-      "/src/assets/html.jpeg",
-      "/src/assets/css.jpeg",
-      "/src/assets/js.jpeg",
-      "/src/assets/bootstrap.jpeg",
-    ],
+    stack: [html, css, js, bootstrap],
   },
   {
     id: 4,
     name: "Landing page of CRAPPO Crypto website",
-    photo: "/src/assets/crypto.png",
+    photo: crypto,
     code: "https://github.com/dandytech/crypto",
     site: "https://dandy-crypto.netlify.app/",
-    stack: [
-      "/src/assets/html.jpeg",
-      "/src/assets/css.jpeg",
-      "/src/assets/js.jpeg",
-    ],
+    stack: [html, css, js],
   },
   {
     id: 5,
     name: "Dashboard for Product Sales Firm",
-    photo: "/src/assets/dashboard.png",
+    photo: dashboard,
     code: "https://github.com/dandytech/dashboard",
     site: "https://dandy-dashboard.netlify.app/",
-    stack: [
-      "/src/assets/html.jpeg",
-      "/src/assets/css.jpeg",
-      "/src/assets/js.jpeg",
-      "/src/assets/bootstrap.jpeg",
-    ],
+    stack: [html, css, js, bootstrap],
   },
   {
     id: 6,
     name: "Admin Dashboard for Lagos Business School",
-    photo: "/src/assets/dashboard1.png",
+    photo: dashboard1,
     code: "https://github.com/dandytech/adminDashboard",
     site: "https://dandy-dashboard1.netlify.app/",
-    stack: [
-      "/src/assets/html.jpeg",
-      "/src/assets/css.jpeg",
-      "/src/assets/js.jpeg",
-      "/src/assets/bootstrap.jpeg",
-    ],
+    stack: [html, css, js, bootstrap],
   },
   {
     id: 7,
     name: "Input Validation/verification Page",
-    photo: "/src/assets/inputverify.png",
+    photo: inputverify,
     code: "https://github.com/dandytech/inputConfirm",
     site: "https://dandy-inputverification.netlify.app/",
-    stack: [
-      "/src/assets/html.jpeg",
-      "/src/assets/css.jpeg",
-      "/src/assets/js.jpeg",
-    ],
+    stack: [html, css, js],
   },
   {
     id: 8,
     name: "Nexted Registration forms",
-    photo: "/src/assets/nextedreg.png",
+    photo: nextedreg,
     code: "https://github.com/dandytech/nextedreg",
     site: "https://dandy-nextedreg.netlify.app/",
-    stack: [
-      "/src/assets/html.jpeg",
-      "/src/assets/css.jpeg",
-      "/src/assets/bootstrap.jpeg",
-    ],
+    stack: [html, css, bootstrap],
   },
   {
     id: 9,
     name: "Digital Architecture unscroll Page",
-    photo: "/src/assets/digitalarch.png",
+    photo: digitalarch,
     code: "https://github.com/dandytech/digitalarch",
     site: "https://dandy-digitalarch.netlify.app/",
-    stack: ["/src/assets/html.jpeg", "/src/assets/css.jpeg"],
+    stack: [html, css],
   },
 ];
 export default projects;
