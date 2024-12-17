d189fb081dec006d0c600b86a6d5dd5a031c7156
amaechinwankwo <damaechi@rocketmail.com>
2024-12-17 16:13:20 +0100

    my latest commit

--- src/projectsData.js
+++ src/projectsData.js
@@ -7,15 +7,54 @@ import dashboard1 from "../src/assets/dashboard1.png";
 import inputverify from "../src/assets/inputverify.png";
 import nextedreg from "../src/assets/nextedreg.png";
 import digitalarch from "../src/assets/digitalarch.png";
+import cyberspacelte from "../src/assets/cyberspacelte.png";
+import kwarajobs from "../src/assets/kwarajobs.png";
+import sharpapp from "../src/assets/sharpapp.png";
+import wideoasis from "../src/assets/wideoasis.png";
 
 import html from "../src/assets/html.jpeg";
 import css from "../src/assets/css.jpeg";
 import js from "../src/assets/js.jpeg";
 import bootstrap from "../src/assets/bootstrap.jpeg";
+import react from "../src/assets/react.png";
+import tailwind from "../src/assets/tailwindcss.png";
+import styledcom from "../src/assets/styledcom.jpeg";
 
 const projects = [
   {
     id: 1,
+    name: "Cyberspace LTE",
+    photo: cyberspacelte,
+    code: "https://github.com/dandytech/cyberspace_lte",
+    site: "https://cyberspacelte.netlify.app/home",
+    stack: [react, tailwind, styledcom],
+  },
+  {
+    id: 2,
+    name: "Kwara Job Portal",
+    photo: kwarajobs,
+    code: "https://github.com/bamo100/kwarajobs_FE",
+    site: "https://kwarajobs.vercel.app/",
+    stack: [react, tailwind, styledcom],
+  },
+  {
+    id: 3,
+    name: "Home Service App",
+    photo: sharpapp,
+    code: "https://github.com/dandytech/sharp-sharp",
+    site: "https://sharpapp.netlify.app/",
+    stack: [react, tailwind, styledcom],
+  },
+  {
+    id: 4,
+    name: "Hotel Reservation/Booking App",
+    photo: wideoasis,
+    code: "https://github.com/dandytech/react-the-wild-oasis",
+    site: "https://dandyhotels.netlify.app/",
+    stack: [react, tailwind, styledcom],
+  },
+  {
+    id: 5,
     name: "Landing page of NITAD website",
     photo: nitadImage,
     code: "https://github.com/dandytech/NITAD",
@@ -23,7 +62,7 @@ const projects = [
     stack: [html, css, js, bootstrap],
   },
   {
-    id: 2,
+    id: 6,
     name: "Landing page of Producemart website",
     photo: producemart,
     code: "https://github.com/dandytech/producemart",
@@ -31,7 +70,7 @@ const projects = [
     stack: [html, css, js, bootstrap],
   },
   {
-    id: 3,
+    id: 7,
     name: "Landing page of Primeparkland website",
     photo: primeparkland,
     code: "https://github.com/dandytech/primeparkland",
@@ -39,7 +78,7 @@ const projects = [
     stack: [html, css, js, bootstrap],
   },
   {
-    id: 4,
+    id: 8,
     name: "Landing page of CRAPPO Crypto website",
     photo: crypto,
     code: "https://github.com/dandytech/crypto",
@@ -47,7 +86,7 @@ const projects = [
     stack: [html, css, js],
   },
   {
-    id: 5,
+    id: 9,
     name: "Dashboard for Product Sales Firm",
     photo: dashboard,
     code: "https://github.com/dandytech/dashboard",
@@ -55,7 +94,7 @@ const projects = [
     stack: [html, css, js, bootstrap],
   },
   {
-    id: 6,
+    id: 10,
     name: "Admin Dashboard for Lagos Business School",
     photo: dashboard1,
     code: "https://github.com/dandytech/adminDashboard",
@@ -63,7 +102,7 @@ const projects = [
     stack: [html, css, js, bootstrap],
   },
   {
-    id: 7,
+    id: 11,
     name: "Input Validation/verification Page",
     photo: inputverify,
     code: "https://github.com/dandytech/inputConfirm",
@@ -71,7 +110,7 @@ const projects = [
     stack: [html, css, js],
   },
   {
-    id: 8,
+    id: 12,
     name: "Nexted Registration forms",
     photo: nextedreg,
     code: "https://github.com/dandytech/nextedreg",
@@ -79,12 +118,13 @@ const projects = [
     stack: [html, css, bootstrap],
   },
   {
-    id: 9,
+    id: 13,
     name: "Digital Architecture unscroll Page",
     photo: digitalarch,
     code: "https://github.com/dandytech/digitalarch",
     site: "https://dandy-digitalarch.netlify.app/",
     stack: [html, css],
   },
+  
 ];
 export default projects;
